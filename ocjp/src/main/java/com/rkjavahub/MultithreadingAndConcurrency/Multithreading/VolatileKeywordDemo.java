2f6d853a5046bc2dd026d3e9f2d58365d03a9113
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-07-15 16:07:09 +0530

    Changes

--- ocjp/src/main/java/com/rkjavahub/MultithreadingAndConcurrency/Multithreading/VolatileKeywordDemo.java
+++ ocjp/src/main/java/com/rkjavahub/MultithreadingAndConcurrency/Multithreading/VolatileKeywordDemo.java
@@ -1,9 +1,27 @@
 package com.rkjavahub.MultithreadingAndConcurrency.Multithreading;
 
-public class VolatileKeywordDemo {
+import static com.rkjavahub.MultithreadingAndConcurrency.Multithreading.VolatileKeywordDemo.flag;
 
-	public static void main(String[] args) {
-		
+class VolatileDemo{
 
-	}
+
+
+}
+
+public class VolatileKeywordDemo extends Thread {
+    public static volatile boolean flag = false;
+    public static void main(String[] args) {
+        VolatileKeywordDemo demo1 = new VolatileKeywordDemo();
+        VolatileKeywordDemo demo2 = new VolatileKeywordDemo();
+        Thread thread1 = new Thread(demo1);
+        Thread thread2 = new Thread(demo2);
+
+        public void run() {
+
+        }
+
+
+        thread1.start();
+        thread2.start();
+    }
 }
