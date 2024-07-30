f7b69d46131e63b959828fd34e19dd750c163710
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-07-30 16:05:29 +0530

    formatting

--- java8/src/main/java/com/rkjavahub/lambda_expression/TestCase3_IntMethodWithStringArgument.java
+++ java8/src/main/java/com/rkjavahub/lambda_expression/TestCase3_IntMethodWithStringArgument.java
@@ -1,28 +1,28 @@
 package com.rkjavahub.lambda_expression;
 
 interface Interf2 {
-	public int length(String s);
+    int length(String s);
 }
 
 class Demo2 implements Interf2 {
 
-	@Override
-	public int length(String s) {
-		return +s.length();
-	}
+    @Override
+    public int length(String s) {
+        return +s.length();
+    }
 }
 
 public class TestCase3_IntMethodWithStringArgument {
-	public static void main(String[] args) {
-		// Calling length() by using traditional approach (Till java 1.7)
-		Interf2 i = new Demo2();
-		System.out.println("String length using traditional approach is " + i.length("Rohit"));
+    public static void main(String[] args) {
+        // Calling length() by using traditional approach (Till java 1.7)
+        Interf2 i = new Demo2();
+        System.out.println("String length using traditional approach is " + i.length("Rohit"));
 
-		// Calling m1() by using lambda expression (Java 1.8)
-		Interf2 i2 = (s) -> {
-			return s.length();
-		};
+        // Calling m1() by using lambda expression (Java 1.8)
+        Interf2 i2 = (s) -> {
+            return s.length();
+        };
 
-		System.out.println(i2.length("Sachin"));
-	}
+        System.out.println("String length using lambda expression approach is " + i2.length("Sachin"));
+    }
 }
--- java8/src/main/java/com/rkjavahub/lambda_expression/TestCase5_MultithreadingRunnableInterface.java
+++ java8/src/main/java/com/rkjavahub/lambda_expression/TestCase5_MultithreadingRunnableInterface.java
@@ -2,48 +2,47 @@ package com.rkjavahub.lambda_expression;
 
 class Task implements Runnable {
 
-	@Override
-	public void run() {
-		for (int i = 0; i < 5; i++) {
-			System.out.print("Child Thread");
-			try {
-				Thread.sleep(1555);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Child Thread");
+            try {
+                Thread.sleep(1555);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
 
 public class TestCase5_MultithreadingRunnableInterface {
 
-	public static void main(String[] args) {
-		// Traditional approach
-		/*
-		 * Runnable r = new Task(); Thread t = new Thread(r); t.start(); }
-		 */
+    public static void main(String[] args) {
+        // Traditional approach
+        /*
+         * Runnable r = new Task(); Thread t = new Thread(r); t.start(); }
+         */
 
-		Runnable r1 = () -> {
-			for (int i = 0; i < 5; i++) {
-				System.out.println("Child thread");
-				try {
-					Thread.sleep(1555);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			;
-		};
+        Runnable r1 = () -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Child thread");
+                try {
+                    Thread.sleep(1555);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
 
-		Thread t1 = new Thread(r1);
-		t1.start();
-		for (int i = 0; i < 5; i++) {
-			System.out.println("Main thread");
-			try {
-				Thread.sleep(1555);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        Thread t1 = new Thread(r1);
+        t1.start();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Main thread");
+            try {
+                Thread.sleep(1555);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
