5e91291151d4a07ad064990534e542051f0650d7
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-08-06 20:06:51 +0530

    comment added

--- dsa/src/main/java/com/rkjavahub/dsa/array/problems/ReverseArray.java
+++ dsa/src/main/java/com/rkjavahub/dsa/array/problems/ReverseArray.java
@@ -4,6 +4,9 @@ Input: original_array[] = {1, 2, 3} Output: array_reversed[] = {3, 2, 1}
 
 Input: original_array[] = {4, 5, 1, 2}
 Output: array_reversed[] = {2, 1, 5, 4}
+
+Time complexity = O(n)
+Space complexity = O(1)
  */
 
 
