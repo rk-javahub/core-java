a42920da51ad068ede8c8ea74cdb60cf1a5cd05f
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-08-29 13:54:53 +0530

    changes

---
+++ dsa/src/main/java/com/rkjavahub/dsa/array/problems/TryStoringStringInArrayDemo.java
@@ -0,0 +1,12 @@
+package com.rkjavahub.dsa.array.problems;
+
+import java.util.Arrays;
+
+public class TryStoringStringInArrayDemo {
+    public static void main(String[] args) {
+        String[] strArray = new String[5];
+        strArray[0] = "Rohit";
+
+        System.out.println(Arrays.toString(strArray));
+    }
+}
--- java8/src/main/java/com/rkjavahub/foreach/forEachDemo.java
+++ java8/src/main/java/com/rkjavahub/foreach/forEachDemo.java
@@ -1,2 +1,14 @@
-package com.rkjavahub.foreach;public class forEachDemo {
+package com.rkjavahub.foreach;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class forEachDemo {
+    public static void main(String[] args) {
+        char[] chaarray = {'a', 'b', 'c', 'd', 'e'};
+        List<String> arrayData = List.of(Arrays.toString(chaarray));
+
+        arrayData.forEach(data -> System.out.println(data.toUpperCase()));
+
+    }
 }
