d16607952b533f5bbc1dd35a8997e742117f9efe
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-08-30 11:19:35 +0530

    changes

--- design-pattern/src/main/java/com/rkjavahub/CreationalPattern/com/pattern/singleton/Logger.java
+++ design-pattern/src/main/java/com/rkjavahub/CreationalPattern/com/pattern/singleton/Logger.java
@@ -1,41 +1,40 @@
 package com.rkjavahub.CreationalPattern.com.pattern.singleton;
 
 public class Logger implements Cloneable {
-	// private constructor so no outside class create object
-	private Logger() {
-	}
+    // Only instance of class
+    private static volatile Logger logger;
+    private String logMessage;
+    // private constructor so no outside class create object
+    private Logger() {
+    }
 
-	// Only instance of class
-	private static volatile Logger logger;
-	private String logMessage;
+    // Return the instance of class which is only access point for outside classes
+    // to access object of Logger class.
+    public static Logger getInstance() {
+        if (logger == null) {
+            synchronized (Logger.class) {
+                if (logger == null) {
+                    logger = new Logger();
+                }
+            }
+        }
+        return logger;
+    }
 
-	// Return the instance of class which is only access point for outside classes
-	// to access object of Logger class.
-	public static Logger getInstance() {
-		if (logger == null) {
-			synchronized (Logger.class) {
-				if (logger == null) {
-					logger = new Logger();
-				}
-			}
-		}
-		return logger;
-	}
+    public String getLogMessage() {
+        return logMessage;
+    }
 
-	public String getLogMessage() {
-		return logMessage;
-	}
+    // logMessage is set only at once.
+    public void setLogMessage(String logMessage) {
+        if (this.logMessage == null) {
+            this.logMessage = logMessage;
+        }
+    }
 
-   // logMessage is set only at once.
-	public void setLogMessage(String logMessage) {
-		if (this.logMessage == null) {
-			this.logMessage = logMessage;
-		}
-	}
-
-	// Disable cloning
-	public Object clone() {
-		return new CloneNotSupportedException("cloning not supported for singleton object...");
-	}
+    // Disable cloning
+    public Object clone() {
+        return new CloneNotSupportedException("cloning not supported for singleton object...");
+    }
 
 }
