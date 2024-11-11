5da2858b50c69c73639fc34a11c266fbd1fb3727
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-11-11 11:08:50 +0530

    changes

--- java8/src/main/java/com/rkjavahub/predicate/CheckEvenOrOdd.java
+++ java8/src/main/java/com/rkjavahub/predicate/CheckEvenOrOdd.java
@@ -4,7 +4,7 @@ import java.util.function.Predicate;
 
 public class CheckEvenOrOdd {
     public static void main(String[] args) {
-        Predicate<Integer> predicate = (integer) -> integer % 2 == 0;
-        System.out.println("Number is even : " + predicate.test(13)); // return true or false
+        Predicate<Integer> predicate = i -> i % 2 == 0;
+        System.out.println("Number is even : " + predicate.test(14)); // return true or false
     }
 }
