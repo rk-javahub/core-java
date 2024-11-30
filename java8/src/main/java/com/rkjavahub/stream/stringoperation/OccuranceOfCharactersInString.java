79cc9ba15433b9d9be75fa6de14ac77b53467fcf
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-11-30 09:57:30 +0530

    changes

--- java8/src/main/java/com/rkjavahub/stream/stringoperation/OccuranceOfCharactersInString.java
+++ java8/src/main/java/com/rkjavahub/stream/stringoperation/OccuranceOfCharactersInString.java
@@ -1,5 +1,6 @@
 package com.rkjavahub.stream.stringoperation;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -8,8 +9,18 @@ public class OccuranceOfCharactersInString {
     public static void main(String[] args) {
         String str = "Rohit is a java developer";
         Map<Character, Long> collect = str.chars().mapToObj(ch -> (char) ch).filter(ch -> !Character.isWhitespace(ch)).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+/*
         for (Map.Entry e : collect.entrySet()) {
-            System.out.println(e.getKey() + " " + e.getValue());
+            System.out.print(e.getKey() + " " + e.getValue());
         }
+        System.out.println();
+*/
+
+        Map<String, Long> collect1 = Arrays.stream(str.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        for (Map.Entry e : collect.entrySet()) {
+            System.out.print(e.getKey() + " " + e.getValue());
+            System.out.println();
+        }
+
     }
 }
