1752aa9b1778efe25916cda549637d9c04bde6dc
Rohit Kumbhar <kumbharrohit13@gmail.com>
2024-11-30 10:59:04 +0530

    changes

--- java_new_features/src/main/java/FilesReadString_JDK11.java
+++ java_new_features/src/main/java/FilesReadString_JDK11.java
@@ -4,14 +4,18 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * 1.Files.readString()
+ * 1.Files.readString()  (JDK11)
  * Returns: a String containing the content read from the file
+ * 2.Files.writeString() (JDK11)
+ * Write a CharSequence to a file
  */
 
-public class FilesReadString {
+public class FilesReadString_JDK11 {
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("F:\\JAVA\\rk-javahub GitHub\\core-java\\java_new_features\\src\\main\\resources\\demo.txt");
         String str = Files.readString(path);
         System.out.println(str);
+        Path newPath = Paths.get("F:\\JAVA\\rk-javahub GitHub\\core-java\\java_new_features\\src\\main\\resources\\demo1.txt");
+        Files.writeString(newPath, str);
     }
 }
---
+++ java_new_features/src/main/java/PredicateNotRunner_JDK11.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * 1.Predicate.not()  (JDK11)
+ * Returns a predicate that is the negation of the supplied predicate.
+ */
+public class PredicateNotRunner_JDK11 {
+
+    public static Boolean checkEven(Integer number) {
+        return number % 2 == 0;
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        System.out.println("Odd Numbers in List are:");
+        numbers.stream().filter(Predicate.not(PredicateNotRunner_JDK11::checkEven)).forEach(System.out::println);
+
+
+    }
+}
