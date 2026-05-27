82652abf46507884fafee3de08a7032abcde5cb1
Rohit Kumbhar <kumbharrohit13@gmail.com>
2026-05-27 10:02:35 +0530

    Changes added and updated

--- java8/src/main/java/com/rkjavahub/stream/stringoperation/HighestLengthWord.java
+++ java8/src/main/java/com/rkjavahub/stream/stringoperation/HighestLengthWord.java
@@ -5,8 +5,15 @@ import java.util.Comparator;
 
 public class HighestLengthWord {
     public static void main(String[] args) {
-        String str = "Hi my name is Rohit Kumbhar";
+        String str = "Hi my name is Rohit Kumbhar rahbmuK";
 
-        Arrays.stream(str.split(" ")).max(Comparator.comparing(String::length)).ifPresent(word -> System.out.println(word));
+        System.out.println("Return first highest length word");
+        Arrays.stream(str.split(" ")).max(Comparator.comparing(String::length)).ifPresent(System.out::println);
+
+
+        System.out.println("Return all highest length word");
+        int maxLength = Arrays.stream(str.split(" ")).mapToInt(String::length).max().orElse(0);
+
+        Arrays.stream(str.split(" ")).filter(s -> s.length() == maxLength).forEach(System.out::println);
     }
 }
